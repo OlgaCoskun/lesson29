7e6a2b38a907e7acdd42416ac8d1f3c6b5c56368
Your Name <you@example.com>
2017-09-12 15:01:15 +0300

    add index.erb

--- app.rb
+++ app.rb
@@ -17,5 +17,5 @@ end
 
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+	erb :index
 end
