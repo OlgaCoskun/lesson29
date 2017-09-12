a3a8748e5a8922f7be30c9b3de5d68ff18b77097
Your Name <you@example.com>
2017-09-12 16:51:14 +0300

    add colorpicker and datetime

--- app.rb
+++ app.rb
@@ -14,9 +14,37 @@ class Barber < ActiveRecord::Base
 
 end
 
+
+
+before do
+    @barbers = Barber.all
+end
+
+
+
 get '/' do
     #@barbers = Barber.all
     #сортировка списка
     @barbers = Barber.order "created_at DESC"
 	erb :index
 end
+
+
+
+
+#добавили форму из урока 21 app.rb
+get '/visit' do
+  erb :visit
+end
+
+
+post '/visit' do
+  @name = params[:name]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
+
+erb "<h2>Спасибо, что Вы к нам записались!</h2>"
+
+  end
